32c0b339554a581cc583e2a01f5855f26c0edf3e
pkindalov <panayotkindalov@gmail.com>
2024-07-14 00:33:54 +0300

    - Added link to edit profile in navigation - Added flash messages from devise in application.html.erb and extracted navigation to _main_nav.html.erb - Used bootstrap classes in edit.html.erb, show.html.erb - Updated routes.rb, users_controller.rb files - Other small changes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show, :profile, :settings, :update]
+  before_action :set_user, only: [:show, :profile, :settings, :update]
+
+  def show
+    # set_user вече задава @user
+  end
+
+  def profile
+    # Профилна страница, може да добавиш съдържание тук
+  end
+
+  def settings
+    # settings view-то използва settings.html.erb
+  end
+
+  def update
+    # respond_to do |format|
+    #   if @user.update(user_params)
+    #     format.html { redirect_to @user, notice: 'User was successfully updated.' }
+    #     format.json { render :show, status: :ok, location: @user }
+    #   end
+    # end
+  end
+
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'users/show'
+  root "home#index"
+  get 'about', to: 'home#about'
+  get 'services', to: 'home#services'
+  get 'contacts', to: 'home#contacts'
+  # get 'users/:id/profile', to: 'users#profile', as: 'user_profile'
+  # get 'users/:id/settings', to: 'users#settings', as: 'user_settings'
+  devise_for :users
+  resources :users, only: [:show]
+  # или
+  # get 'users/:id', to: 'users#show', as: :user_profile
+end

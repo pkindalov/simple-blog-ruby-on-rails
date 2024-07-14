5cdb10e1009472e0ccc511abd9c3080cd38a9a4a
pkindalov <panayotkindalov@gmail.com>
2024-07-14 15:16:19 +0300

    - Added will_paginate and will_paginate-bootstrap gems - Added paginate.css and include it in application.html.erb Other small changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.paginate(:page => params[:page], :per_page => 3).order('created_at DESC')
   end
 
   def about

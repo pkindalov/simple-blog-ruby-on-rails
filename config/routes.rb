c8cc4382c96e1e11f3ae39f0ea2f8a80b97f2088
pkindalov <panayotkindalov@gmail.com>
2024-07-17 00:11:22 +0300

    - Formatting code

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,10 @@ Rails.application.routes.draw do
   # get 'users/:id/settings', to: 'users#settings', as: 'user_settings'
   get 'users/:id/profile', to: 'users#profile', as: 'user_profile'
 
-
   devise_for :users
   resources :users, only: [:show]
   resources :posts, only: %i[index new create edit update destroy show]
   delete 'posts/:id/photo/:photo_id', to: 'posts#delete_photo', as: 'delete_post_photo'
-
 
   # или
   # get 'users/:id', to: 'users#show', as: :user_profile

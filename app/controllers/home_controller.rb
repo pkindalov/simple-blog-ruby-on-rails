4d6317ff57fdecca2923e4d07291e0ba6f3ec37e
pkindalov <panayotkindalov@gmail.com>
2024-07-18 14:31:48 +0300

    - Updated filter select adding one more option - to show most liked posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,8 @@ class HomeController < ApplicationController
       @posts = Post.paginate(page: params[:page], per_page: 3).order(views_count: :desc)
     when 'photos'
       @posts = Post.left_joins(:photos_attachments).group('posts.id').paginate(page: params[:page], per_page: 3).order('COUNT(active_storage_attachments.record_id) DESC').where(active_storage_attachments: { name: 'photos', record_type: 'Post' })
+    when 'likes'
+      @posts = Post.left_joins(:likes).group('posts.id').paginate(page: params[:page], per_page: 3).order('COUNT(likes.id) DESC')
     else
       # 'recent' or default case
       @posts = Post.paginate(page: params[:page], per_page: 3).order(created_at: :desc)

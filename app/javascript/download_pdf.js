a09226e3e617d305e162567ae631a77f6f7324dc
pkindalov <panayotkindalov@gmail.com>
2024-07-30 13:33:03 +0300

    - Small refactoring on download pdf buttons and download_pdf.js

--- app/javascript/download_pdf.js
+++ app/javascript/download_pdf.js
@@ -1,13 +1,19 @@
-document.addEventListener('turbo:load', function() {
+document.addEventListener('turbo:load', function () {
     const downloadButtons = document.querySelectorAll('.download-pdf-button');
     downloadButtons.forEach(button => {
-        button.addEventListener('click', async function(event) {
-            const postId = this.getAttribute('data-post-id');
+        button.addEventListener('click', async function (event) {
+            const url = this.getAttribute('data-url');
             const originalText = this.textContent;
+
+            if (!url) {
+                displayError('Invalid URL provided.');
+                return;
+            }
+
             this.textContent = 'Loading...';
             this.disabled = true;
 
-            await fetch(`/posts/${postId}/download_pdf`, {
+            await fetch(url, {
                 method: 'GET'
             })
                 .then(response => {
@@ -17,31 +23,33 @@ document.addEventListener('turbo:load', function() {
                     throw new Error('Network response was not ok.');
                 })
                 .then(blob => {
-                    const url = window.URL.createObjectURL(blob);
+                    const downloadUrl = window.URL.createObjectURL(blob);
                     const a = document.createElement('a');
-                    a.href = url;
-                    a.download = `post_${postId}.pdf`;
+                    a.href = downloadUrl;
+                    a.download = `post_${this.getAttribute('data-post-id')}.pdf`;
                     document.body.appendChild(a);
                     a.click();
                     a.remove();
-                    window.URL.revokeObjectURL(url);
+                    window.URL.revokeObjectURL(downloadUrl);
                     this.textContent = originalText;
                     this.disabled = false;
-
                 })
                 .then(() => {
-                    window.location.reload()
+                    window.location.reload();
                 })
                 .catch(error => {
-                    const errorElement = document.createElement('p');
-                    errorElement.textContent = error?.message;
-                    errorElement.setAttribute('class', 'alert alert-danger');
-                    document.body.prepend(errorElement);
-                    console.error('Error:', error);
-                    this.textContent = 'Download PDF';
-                    this.disabled = false;
+                    displayError(error?.message, button);
                 });
         });
-
     });
+
+    function displayError(message, button) {
+        const errorElement = document.createElement('p');
+        errorElement.textContent = message;
+        errorElement.setAttribute('class', 'alert alert-danger');
+        document.body.prepend(errorElement);
+        console.error('Error:', message);
+        button.textContent = 'Download PDF';
+        button.disabled = false;
+    }
 });

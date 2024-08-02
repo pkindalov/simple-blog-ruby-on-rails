a7cee049ddce42e3a38bc763d9703145200255f1
pkindalov <panayotkindalov@gmail.com>
2024-08-03 01:48:29 +0300

    - Shown on the main view home/index.html.erb post from the followed users

--- app/controllers/concerns/sortable.rb
+++ app/controllers/concerns/sortable.rb
@@ -10,7 +10,8 @@ module Sortable
   def load_sorted_posts
     @sort_by = params[:sort_by] || 'popular'
 
-    @posts = Rails.cache.fetch("#{@sort_by}/posts/#{params[:page]}", expires_in: 10.minutes) do
+    # Извличане на всички постове
+    @posts = Rails.cache.fetch("#{@sort_by}/all_posts/#{params[:page]}", expires_in: 10.minutes) do
       case @sort_by
       when 'popular'
         Post.select("posts.*, (COALESCE(posts.views_count, 0) + COALESCE(posts.downloaded_as_pdf, 0) + COALESCE(COUNT(likes.id), 0) + COALESCE(COUNT(comments.id), 0)) AS popularity_score")
@@ -48,6 +49,57 @@ module Sortable
         Post.paginate(page: params[:page], per_page: 5).order(created_at: :desc)
       end
     end
-  end
 
+    # Извличане на постове на последваните потребители
+    following_ids = current_user.following.pluck(:id)
+
+    @following_posts = Rails.cache.fetch("#{@sort_by}/following_posts/#{params[:page]}", expires_in: 10.minutes) do
+      case @sort_by
+      when 'popular'
+        Post.where(user_id: following_ids)
+            .select("posts.*, (COALESCE(posts.views_count, 0) + COALESCE(posts.downloaded_as_pdf, 0) + COALESCE(COUNT(likes.id), 0) + COALESCE(COUNT(comments.id), 0)) AS popularity_score")
+            .left_joins(:likes, :comments)
+            .group('posts.id')
+            .order('popularity_score DESC')
+            .paginate(page: params[:page], per_page: 5)
+      when 'views'
+        Post.where(user_id: following_ids)
+            .paginate(page: params[:page], per_page: 5)
+            .order(views_count: :desc)
+      when 'photos'
+        Post.where(user_id: following_ids)
+            .left_joins(:photos_attachments)
+            .group('posts.id')
+            .paginate(page: params[:page], per_page: 5)
+            .order('COUNT(active_storage_attachments.record_id) DESC')
+            .where(active_storage_attachments: { name: 'photos', record_type: 'Post' })
+      when 'likes'
+        Post.where(user_id: following_ids)
+            .left_joins(:likes)
+            .group('posts.id')
+            .paginate(page: params[:page], per_page: 5)
+            .order('COUNT(likes.id) DESC')
+      when 'comments'
+        Post.where(user_id: following_ids)
+            .left_joins(:comments)
+            .group('posts.id')
+            .paginate(page: params[:page], per_page: 5)
+            .order('COUNT(comments.id) DESC')
+      when 'author_asc'
+        Post.where(user_id: following_ids)
+            .joins(:user)
+            .paginate(page: params[:page], per_page: 5)
+            .order('users.email ASC')
+      when 'author_desc'
+        Post.where(user_id: following_ids)
+            .joins(:user)
+            .paginate(page: params[:page], per_page: 5)
+            .order('users.email DESC')
+      else
+        Post.where(user_id: following_ids)
+            .paginate(page: params[:page], per_page: 5)
+            .order(created_at: :desc)
+      end
+    end
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,27 +3,7 @@
 class HomeController < ApplicationController
   include Sortable
   def index
-    # @sort_by = params[:sort_by] || 'recent'
-    #
-    # @posts = case @sort_by
-    #          when 'views'
-    #            Post.paginate(page: params[:page], per_page: 3).order(views_count: :desc)
-    #          when 'photos'
-    #            Post.left_joins(:photos_attachments)
-    #                .group('posts.id').paginate(page: params[:page], per_page: 3)
-    #                .order('COUNT(active_storage_attachments.record_id) DESC')
-    #                .where(active_storage_attachments: { name: 'photos', record_type: 'Post' })
-    #          when 'likes'
-    #            Post.left_joins(:likes)
-    #                .group('posts.id')
-    #                .paginate(page: params[:page], per_page: 3)
-    #                .order('COUNT(likes.id) DESC')
-    #          else
-    #            # 'recent' or default case
-    #            Post.paginate(page: params[:page], per_page: 3).order(created_at: :desc)
-    #          end
 
-    # @posts = Post.paginate(:page => params[:page], :per_page => 3).order('created_at DESC')
   end
 
   def about; end

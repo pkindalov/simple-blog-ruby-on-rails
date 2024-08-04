9236262e4cbb8668296362368af93866779e6c4e
pkindalov <panayotkindalov@gmail.com>
2024-08-04 11:28:59 +0300

    - Added block user functionality logic in controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_post
   before_action :set_comment, only: %i[edit update destroy]
+  before_action :check_blocked, only: %i[create edit update destroy]
+
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
     @comment.user = current_user
 
@@ -16,7 +18,7 @@ class CommentsController < ApplicationController
   end
 
   def edit
-
+    # Логиката за редактиране е вече обработена от before_action
   end
 
   def update
@@ -45,4 +47,20 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content)
   end
+
+  def check_blocked
+    if current_user.blocking?(@post.user) || @post.user.blocking?(current_user)
+      redirect_to post_path(@post), alert: 'You cannot interact with this post.'
+    end
+  end
+
+  # if want user to edit or delete comments in blocked user's posts
+  # def check_blocked
+  #   if current_user.blocking?(@post.user) || @post.user.blocking?(current_user)
+  #     if action_name.in?(%w[edit update destroy]) && @comment.user == current_user
+  #       return # Разрешава редактиране и изтриване само на собствени коментари
+  #     end
+  #     redirect_to post_path(@post), alert: 'You cannot interact with this post.'
+  #   end
+  # end
 end
